0ecc7401a03e0492e51a9268de60feb3b3f09f7f
Alex Siryi <desertkun@gmail.com>
2018-05-08 16:45:43 +0300

    Renamed project to steamworks4r

--- java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
+++ java-wrapper/src/main/java/com/codedisaster/steamworks/Version.java
@@ -2,7 +2,7 @@ package com.codedisaster.steamworks;
 
 public final class Version {
 
-	private static final String VERSION = "1.7.1-SNAPSHOT";
+	private static final String VERSION = "1.7.2-ru";
 
 	public static String getVersion() {
 		return VERSION;
